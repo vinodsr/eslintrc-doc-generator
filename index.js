c11b484335cb4b8e30996c026f65468d7c9c682d
Vinod <smartvinu@gmail.com>
2017-09-04 19:19:19 +0530

    FIX

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-var eslint = require("./a.json");
-var request = require("request");
+
+const request = require("request");
+const Handlebars = require('handlebars')
 const getRuleURI = require('eslint-rule-documentation');
+const async = require('async')
 
 var response = "";
 
-for(rule in eslint.rules) {
-	status = getRuleURI(rule);
+fs = require('fs')
+var eslint = fs.readFileSync('./eslintrc.json', 'utf8');
+var templatesource = fs.readFileSync('./template.input', 'utf8');
+eslint = JSON.parse(eslint)
+//var eslint = require("./eslintrc.json");
+// Find out the eslint template for
+
+
+var template = Handlebars.compile(templatesource);
+
+var templatedata = {
+	rules : []
+}
+console.log('Generating...')
+async.forEachOf(eslint.rules,function (key,value,callback) {
+	status = getRuleURI(value);
 	if(status.found == true) {
 		if(status.url.startsWith("http://eslint.org/")) {
 			//replace the url with the github one.
@@ -15,21 +31,28 @@ for(rule in eslint.rules) {
 		}
 		if(status.url.endsWith(".md")) {
 		request(status.url,function (error, response, body)  {
-			console.log(body)
-			console.log()
-			response += body;
-			response +="\n";
+			//console.log(body)
+			//console.log()
+			//response += body;
+			//response +="\n";
+			templatedata.rules.push({
+				rule: value,
+				ruledefinition: body
+			})
+			callback()
 			
 		})
 		
 	}
-		console.log("rule:",rule," " , status.url);
+		//console.log("rule:",rule," " , status.url);
 	} else {
 		console.err(rule," not found");
 		process.exit(-1)
 	}
-	
-	
-}
+},function (err) {
+console.log('Completed',err)
+var result = template(templatedata);
+fs.writeFileSync('out.md',result)
+})
 
 
